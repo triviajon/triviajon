c1bf2b81a5832cdb892d98c6fa6e0c3203de74b7
Jon <johnman10109@gmail.com>
2022-01-08 20:55:30 -0500

    update to js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
-/* ©2014 - 2021 CodyHouse. A project by @romano_cla and @guerriero_se */
+$("img.github").click(function(){
+	window.location.href = "https://github.com/triviajon/";
+});
 
+$("img.linkedin").click(function(){
+	window.location.href = "https://www.linkedin.com/in/jon-rosario-6330741b2/";
+});
+
+$("img.email").click(function(){
+	window.location.href = "mailto:theli@mit.edu";
+});
+
+$("img.resume").click(function(){
+	window.location.href = "content/resume.pdf";
+});
+
+$("img.about").click(function(){
+	window.location.href = "https://jon.mit.edu/";
+});
+
+document.body.style.cursor = 'none';
+
+$('img').on('dragstart', function(event) { event.preventDefault(); });
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove', function(update) {
+    update.stopPropagation();
+    var x = update.clientX;
+    var y = update.clientY;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+});
+
+/* Adapted from ©2014 - 2021 CodyHouse. */
 jQuery(document).ready(function($){
 	//define store some initial variables
 	var	halfWindowH = $(window).height()*0.5,
 		halfWindowW = $(window).width()*0.5,
-		//define a max rotation value (X and Y axises)
 		maxRotationY = 5,
 		maxRotationX = 3,
 		aspectRatio;
 
-	//detect if hero <img> has been loaded and evaluate its aspect-ratio
 	$('.vis-floating-background').find('img').eq(0).load(function() {
 		aspectRatio = $(this).width()/$(this).height();
   		if( $('html').hasClass('preserve-3d') ) initBackground();
 	}).each(function() {
-		//check if image was previously load - if yes, trigger load event
   		if(this.complete) $(this).load();
 	});
 	
-	//detect mouse movement
 	$('.vis-background-wrapper').each(function(){
 		$(this).on('mousemove', function(event){
 			var wrapperOffsetTop = $(this).offset().top;
@@ -30,7 +57,6 @@ jQuery(document).ready(function($){
 		});
 	});
 
-	//on resize - adjust .vis-background-wrapper and .vis-floating-background dimentions and position
 	$(window).on('resize', function(){
 		if( $('html').hasClass('preserve-3d') ) {
 			window.requestAnimationFrame(function(){
@@ -52,11 +78,9 @@ jQuery(document).ready(function($){
 			newLeft = halfWindowW - newImageWidth/2,
 			newTop = (wrapperHeight - newImageHeight)/2;
 
-		//set an height for the .vis-background-wrapper
 		$('.vis-background-wrapper').css({
 			'height' : wrapperHeight,
 		});
-		//set dimentions and position of the .vis-background-wrapper		
 		$('.vis-floating-background').addClass('is-absolute').css({
 			'left' : newLeft,
 			'top' : newTop,
@@ -84,8 +108,6 @@ jQuery(document).ready(function($){
 	}
 });
 
-/* 	Detect "transform-style: preserve-3d" support, or update csstransforms3d for IE10 ? #762
-	https://github.com/Modernizr/Modernizr/issues/762 */
 (function getPerspective(){
   var element = document.createElement('p'),
       html = document.getElementsByTagName('html')[0],
@@ -120,32 +142,3 @@ jQuery(document).ready(function($){
 
 })();
 
-$("img.github").click(function(){
-	window.location.href = "https://github.com/triviajon/";
-});
-
-$("img.linkedin").click(function(){
-	window.location.href = "https://www.linkedin.com/in/jon-rosario-6330741b2/";
-});
-
-$("img.email").click(function(){
-	window.location.href = "mailto:theli@mit.edu";
-});
-
-$("img.resume").click(function(){
-	window.location.href = "content/resume.pdf";
-});
-
-document.body.style.cursor = 'none';
-
-$('img').on('dragstart', function(event) { event.preventDefault(); });
-
-
-var cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', function(e) {
-    e.stopPropagation();
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-});
