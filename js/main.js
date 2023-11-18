54dd398bf9212a82a2ea988875fa8c89a6a53672
Jon Rosario <jonf.rosario@gmail.com>
2023-11-18 13:18:04 -0500

    Fixing dark mode

--- js/main.js
+++ js/main.js
@@ -1,40 +1,53 @@
 let dark_mode_status;
-dark_mode_status = localStorage.getItem('dark_mode_status');
+dark_mode_status = localStorage.getItem("dark_mode_status");
 
-if (dark_mode_status === 'on') {
-    var body = document.body;
-    body.classList.toggle('dark-body');
-    var images = document.getElementsByTagName("img");
-    var i;
-    console.log(images)
-    for (i=0; i<images.length; i++) {
-        if (!("no_invert" in images[i].classList)) {
-            images[i].classList.toggle('invertible');
-        }
+if (dark_mode_status === "on") {
+  var body = document.body;
+  body.classList.toggle("dark-body");
+  var images = document.getElementsByTagName("img");
+  var i;
+  console.log(images);
+  for (i = 0; i < images.length; i++) {
+    if (!("no_invert" in images[i].classList)) {
+      images[i].classList.toggle("invertible");
     }
+  }
 
-    document.getElementById("brightness_1").innerHTML = "dark?";
-    document.getElementById("brightness_2").innerHTML = "light mode.";
+  document.getElementById("brightness_1").innerHTML = "dark?";
+  document.getElementById("brightness_2").innerHTML = "light mode.";
 }
 
 function ToggleDarkMode() {
-    var body = document.body;
-    body.classList.toggle('dark-body');
-    var images = document.getElementsByTagName("img");
-    var i;
-    for (i=0; i<images.length; i++) {
-        images[i].classList.toggle('invertible');
-    }
+  var body = document.body;
+  body.classList.toggle("dark-body");
+  var images = document.getElementsByTagName("img");
+  var i;
+  for (i = 0; i < images.length; i++) {
+    images[i].classList.toggle("invertible");
+  }
 
+  if (localStorage.getItem("dark_mode_status") === "off") {
+    document.getElementById("brightness_1").innerHTML = "dark?";
+    document.getElementById("brightness_2").innerHTML = "light mode.";
+    localStorage.setItem("dark_mode_status", "on");
+  } else {
+    document.getElementById("brightness_1").innerHTML = "bright?";
+    document.getElementById("brightness_2").innerHTML = "dark mode.";
+    localStorage.setItem("dark_mode_status", "off");
+  }
 
-    if (localStorage.getItem("dark_mode_status") === 'off') {
-        document.getElementById("brightness_1").innerHTML = "dark?";
-        document.getElementById("brightness_2").innerHTML = "light mode.";
-        localStorage.setItem("dark_mode_status", 'on');
+  var iframe = document.querySelector("iframe");
+  console.log(iframe);
+  if (iframe) {
+    var iframeDocument =
+      iframe.contentDocument || iframe.contentWindow.document;
+    iframeDocument.body.classList.toggle("dark-body");
+
+    console.log(iframeDocument)
+
+    var iframeImages = iframeDocument.getElementsByTagName("img");
+    for (i = 0; i < iframeImages.length; i++) {
+      iframeImages[i].classList.toggle("invertible");
     }
-    else {
-        document.getElementById("brightness_1").innerHTML = "bright?";
-        document.getElementById("brightness_2").innerHTML = "dark mode.";
-        localStorage.setItem("dark_mode_status", 'off');
-    }
+  }
 }
