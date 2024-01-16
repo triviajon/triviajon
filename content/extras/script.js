3125566fdd1fa602c4542dbff558fc213d247908
Jon Rosario <jonf.rosario@gmail.com>
2024-01-16 16:52:07 -0500

    Changed Journal to Extras

--- content/extras/script.js
+++ content/extras/script.js
@@ -1,16 +1,48 @@
+// See https://fontawesome.com/search?o=r&m=free&s=solid
+const JOURNAL_ICON = "fa-pencil";
+const PUBLISHED_ICON = "fa-book";
+const MUSIC_ICON = "fa-music";
+const CODE_ICON = "fa-code";
+
+function createIcons(article) {
+    const dataTypes = article.getAttribute('data-types').split(' ');
+    
+    dataTypes.forEach(type => {
+        const icon = document.createElement('span');
+        icon.className = 'icon';
+        icon.setAttribute('data-type', type);
+
+        const iconContent = document.createElement('i');
+        iconContent.className = 'fas';
+        switch (type) {
+            case 'journal':
+                iconContent.classList.add(JOURNAL_ICON);
+                break;
+            case 'published':
+                iconContent.classList.add(PUBLISHED_ICON);
+                break;
+            case 'music':
+                iconContent.classList.add(MUSIC_ICON);
+                break;
+            case 'code':
+                iconContent.classList.add(CODE_ICON);
+                break;
+        }
+
+        icon.appendChild(iconContent);
+        article.insertBefore(icon, article.firstChild);
+    });
+}
+
 function filterArticles() {
-    const selectedTags = Array.from(document.querySelectorAll('#tag-filters input:checked')).map(tag => tag.value);
+    const selectedTags = Array.from(document.querySelectorAll('.tag-filter.selected')).map(tag => tag.id);
     const articles = document.querySelectorAll('#article-list .article');
 
-    console.log(selectedTags)
-
     articles.forEach(article => {
-        const articleType = article.getAttribute('data-type');
-        if (selectedTags.includes(articleType) || selectedTags.length === 0) {
-            article.style.display = 'block';
-        } else {
-            article.style.display = 'none';
-        }
+        const articleTypes = article.getAttribute('data-types').split(' ');
+        console.log(article, articleTypes);
+        const displayArticle = selectedTags.length === 0 || selectedTags.some(tag => articleTypes.includes(tag));
+        article.style.display = displayArticle ? 'block' : 'none';
     });
 }
 
@@ -18,3 +50,12 @@ function toggleTag(tagElement) {
     tagElement.classList.toggle('selected');
     filterArticles();
 }
+
+// Run once page is done loading:
+document.addEventListener('DOMContentLoaded', () => {
+    const articles = document.querySelectorAll('#article-list .article');
+    articles.forEach(article => {
+        console.log(article);
+        createIcons(article);
+    }
+)});
